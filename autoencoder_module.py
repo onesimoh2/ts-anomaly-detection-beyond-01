a6c222a6e191c7915cc9e83d144b200fb889b189
Onesimo Hernandez <ecbeyond@hotmail.com>
2022-04-01 22:11:48 -0400

    Add autoencoder

--- autoencoder_module.py
+++ autoencoder_module.py
@@ -65,9 +65,11 @@ class FeatureDatasetFromDf(Dataset):
             self.x_train[:,[0,1,2]] = scaler.transform(self.x_train[:,[0,1,2]].reshape(-1, 3)).reshape(-1, 3)               
 
         #biasAddedSeed = [((item[0]*item[0]) + (item[1]*item[1])  + (item[2]*item[2])) for item in self.x_train]
-        biasAdded = [np.abs(item[0]-item[1]) for item in self.x_train]
-        biasAddedR = np.array(biasAdded).reshape(-1, 1)
-        self.x_train = np.append(self.x_train, biasAddedR, axis=1)
+        data_vs_trend = [np.abs(item[0]-item[1]) for item in self.x_train]
+        data_vs_trend_reshape = np.array(data_vs_trend).reshape(-1, 1)
+        self.x_train = np.append(self.x_train, data_vs_trend_reshape, axis=1)
+
+        #x_train have: data, train estimate, stationary estimate and difference between the first two
 
         #trainn.to_csv('C:/Users/ecbey/Downloads/x_train.csv')  
         self.X_train = torch.tensor(self.x_train, dtype=torch.float32)
@@ -77,3 +79,68 @@ class FeatureDatasetFromDf(Dataset):
     
     def __getitem__(self, idx):
         return self.X_train[idx]
+
+
+class autoencoder(nn.Module):
+
+    #iitialize weights
+    def initialize_weights(self):
+        for n in self.modules():
+            if isinstance(n, nn.Linear):
+                nn.init.kaiming_uniform_(n.weight)
+                nn.init.constant_(n.bias, 0)
+
+
+    def __init__(self, epochs=15, batchSize=10, learningRate=1e-3, weight_decay=1e-5, layer_reduction_factor = 1.6, number_of_features = 29, seed=15000):
+        super(autoencoder, self).__init__()
+        #seed = 15000
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+        self.epochs = epochs
+        self.batchSize = batchSize
+        self.learningRate = learningRate
+        self.weight_decay = weight_decay
+        self.layer_reduction_factor = layer_reduction_factor
+        self.number_0f_features = number_of_features
+        self.output_features_by_leyers = []
+        self.output_features_by_leyers.append(number_of_features)
+        self.number_of_last_layer = int(round(number_of_features / 6.0))
+        if self.number_of_last_layer <= 1 and number_of_features > 3:
+            self.number_of_last_layer = 2
+
+        for leyer_lev in range(5):
+            reduct = int(round(self.output_features_by_leyers[leyer_lev] / layer_reduction_factor))
+            if reduct < self.number_of_last_layer or reduct == self.output_features_by_leyers[-1] or reduct == 0:
+                break
+            last_leyer = self.output_features_by_leyers[-1]
+            self.output_features_by_leyers.append(reduct)
+            if(reduct == 1):
+                break
+
+
+
+        self.encoder = nn.Sequential()
+
+        for iLayer in range(len(self.output_features_by_leyers)-1):
+            self.encoder.add_module('L' + str(iLayer), nn.Linear(self.output_features_by_leyers[iLayer], self.output_features_by_leyers[iLayer + 1]))
+            if iLayer + 1 < len(self.output_features_by_leyers) - 1:
+                self.encoder.add_module('R' + str(iLayer), nn.ReLU())
+        
+        self.decoder = nn.Sequential()
+        for iLayer in range(len(self.output_features_by_leyers))[::-1]:
+            self.decoder.add_module('L' + str(iLayer), nn.Linear(self.output_features_by_leyers[iLayer], self.output_features_by_leyers[iLayer - 1]))
+            if iLayer > 1:
+                self.decoder.add_module('R' + str(iLayer), nn.ReLU())
+            else:
+                break
+
+        self.initialize_weights()
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learningRate, weight_decay=self.weight_decay)
+        
+        #self.optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.9)
+        
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, patience=5, factor=0.1, mode='min', verbose=True)
+        self.loss = nn.MSELoss()
+        #self.loss = nn.L1Loss()
--- catfish_first_part.py
+++ catfish_first_part.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from statistics import mean, median
 from sklearn.preprocessing import MinMaxScaler
 from fft_functions import fourier_extrapolation
-from autoencoder_module import FeatureDatasetFromDf
+from autoencoder_module import FeatureDatasetFromDf, autoencoder
 
 
 
@@ -64,5 +64,22 @@ def catfich_1986_2001(train_test) :
     scaler = MinMaxScaler()
     n_df = len(train_db)
     train_split = FeatureDatasetFromDf(train_db, scaler, 'true', COLUMN_NAMES, DATE_COLUMN_NAME, 1, n_df, xrandPosition)
+    ipos = train_split.iPos
+    extrpl, x_freqdom, f, p, indexes, n_train = train_split.extrpl, train_split.x_freqdom, train_split.f, train_split.p, train_split.indexes, train_split.n_train
+
+    if train_test:
+        test_split = FeatureDatasetFromDf(test_db, scaler, 'false', COLUMN_NAMES, DATE_COLUMN_NAME, ipos, n_df, extrpl, x_freqdom, f, p, indexes, n_train)
+        ipos = test_split.iPos
+
+    MAX_TRAINING_LOSS_VAR =  3.0 #number of sigmas to consider anomaly
+    #LAYER_REDUCTION_FACTOR = 1.6 #how much each leyer of the autoencoder decrease
+    LAYER_REDUCTION_FACTOR = 1.2 #how much each leyer of the autoencoder decrease
+    BATCH_SIZE = int(len(train_db)/10) 
+    
+
+    number_of_features = int(train_split.X_train.size(dim=1))
+
+    model = autoencoder(epochs = 500, batchSize = BATCH_SIZE, number_of_features = number_of_features, layer_reduction_factor = LAYER_REDUCTION_FACTOR,  seed = seed)
+
 
 
